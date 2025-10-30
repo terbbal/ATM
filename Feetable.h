d9ce67a82a1035c703142c236131749258e97ed5
terbbal <terbbal310@gmail.com>
2025-10-30 23:32:23 +0900

    Update Feetable.h

--- Feetable.h
+++ Feetable.h
@@ -4,8 +4,8 @@ using namespace std;
 //REQ1.8 Each ATM have several types of transcation fees.
 
 class Feetable{
-static bouble getDepositFee(bool isprimary); //true이면 0원, false이면 1000원
-static bouble getWithdrawalFee(bool isprimary); //true이면 1000원. false이면 2000원
-static bouble getTransferFee(bool fromprimary, bool toprimary); // true-true 1000원, true-false or false-true 2000원, flase-false 4000원
-static bouble getCasgTreansferFee(); // 무조건 2000원
+static double getDepositFee(bool isprimary); //true이면 0원, false이면 1000원
+static double getWithdrawalFee(bool isprimary); //true이면 1000원. false이면 2000원
+static double getTransferFee(bool fromprimary, bool toprimary); // true-true 1000원, true-false or false-true 2000원, flase-false 4000원
+static double getCasgTreansferFee(); // 무조건 2000원
 };
