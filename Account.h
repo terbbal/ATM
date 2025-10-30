dee623bf84045f658457def88c1e6ab3d68d9e2a
terbbal <terbbal310@gmail.com>
2025-10-30 23:42:28 +0900

    Update Account.h

--- Account.h
+++ Account.h
@@ -16,8 +16,9 @@ string getuserName() const;
 string getaccountNumber() const;
 double getbalance() const;
 
-void deposit(double amount);
-void withdraw(double amount);
+void deposit(double amount); //REQ 4.3 Once Cash or checks are accepted by ATM, the transaction must be reflected to the bank account as well
+void withdraw(double amount); //REQ 5.3 출금이 성공하면 계좌에도 출금이 반영되어야 함.
+// 추가적으로 6.7 만족
 
 void display() const;
 };
